e6979fc50812ee83c336e03480f06976cd35e0c5
DongHyeon Kim <hyeoni369@naver.com>
2022-07-07 03:48:46 +0900

    send 부모객체로 뺌

--- dongsocket/client.py
+++ dongsocket/client.py
@@ -11,13 +11,5 @@ class Client(DongSocket):
     def disconnect(self):
         self.send(DongSocket.DISCONNECT_MESSAGE)
 
-    def send(self, data: any):
-        encoded_data = data.encode(DongSocket.FORMAT)
-        encoded_data_length = len(encoded_data)
-        encoded_encoded_data_length = str(encoded_data_length).encode(DongSocket.FORMAT)
-        data_length_header = encoded_encoded_data_length.ljust(DongSocket.HEADER, b' ')
-        self.client.send(data_length_header)
-        self.client.send(encoded_data)
-
     def __del__(self):
         self.disconnect()
--- dongsocket/dongsocket.py
+++ dongsocket/dongsocket.py
@@ -1,3 +1,4 @@
+import json
 import socket
 
 
@@ -10,3 +11,12 @@ class DongSocket:
     def __init__(self, server_ip: str):
         self.server_ip: str = server_ip
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def send(self, data: any):
+        serialized_data = json.dumps(data)
+        encoded_data = serialized_data.encode(DongSocket.FORMAT)
+        encoded_data_length = len(encoded_data)
+        encoded_encoded_data_length = str(encoded_data_length).encode(DongSocket.FORMAT)
+        data_length_header = encoded_encoded_data_length.ljust(DongSocket.HEADER, b' ')
+        self.socket.send(data_length_header)
+        self.socket.send(encoded_data)
