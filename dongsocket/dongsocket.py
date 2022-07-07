baafe6d1d2b785f952a0c04b99dd812d84cc1c0f
DongHyeon Kim <hyeoni369@naver.com>
2022-07-08 00:34:58 +0900

    send 할 때도 connection 이용할 수 있도록 함

--- dongsocket/dongsocket.py
+++ dongsocket/dongsocket.py
@@ -12,14 +12,16 @@ class DongSocket:
         self.server_ip: str = server_ip
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def send(self, data: any):
+    def send(self, data: any, connection: socket = None):
+        conn = connection if connection is not None else self.socket
+
         serialized_data = json.dumps(data)
         encoded_data = serialized_data.encode(DongSocket.FORMAT)
         encoded_data_length = len(encoded_data)
         encoded_encoded_data_length = str(encoded_data_length).encode(DongSocket.FORMAT)
         data_length_header = encoded_encoded_data_length.ljust(DongSocket.HEADER, b' ')
-        self.socket.send(data_length_header)
-        self.socket.send(encoded_data)
+        conn.send(data_length_header)
+        conn.send(encoded_data)
 
     def receive(self, connection: socket = None) -> any:
         conn = connection if connection is not None else self.socket
